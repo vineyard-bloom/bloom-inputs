3c74944fea03f43cddb394f1f9b65233c5a7eec0
Katie Foster <fosterkatiea@gmail.com>
2018-04-12 12:08:43 -0600

    gutting example

--- example/src/components/presentation/example.jsx
+++ example/src/components/presentation/example.jsx
@@ -1,50 +1,10 @@
 import React from 'react'
-import {
-  Accordion,
-  Alert,
-  BannerAlert,
-  Loading,
-  Modal,
-  Table,
-  Tooltip
-} from 'bloom-layout'
 
-const Example = ({ activeAlert, activeBanner, closeAlert, closeBanner, closeModal, modalContents, openAlert, openBanner, openModal }) => {
-  const accordionSections = [
-    {
-      contents: <div>I am contents</div>,
-      header: 'Section 1'
-    },
-    {
-      contents: <div>I am more contents</div>,
-      header: 'Section 2'
-    }
-  ]
+const Example = (props) => {
 
   return (
     <div>
-      <BannerAlert
-        closeBanner={closeBanner}
-        currentBanner={activeBanner}
-      />
-      { !activeBanner &&
-        <button onClick={openBanner}>
-          reopen banner
-        </button>
-      }
-      <Accordion
-        sections={accordionSections}
-      />
-      <button onClick={openAlert}>Open Alert</button>
-      <button onClick={openModal}>Open Modal</button>
-      <Modal modalContents={modalContents}
-        closeModal={closeModal}
-      />
-      <Alert
-        closeAlert={closeAlert}
-        currentAlert={activeAlert}
-        hidden={!activeAlert}
-      />
+
     </div>
   )
 }
